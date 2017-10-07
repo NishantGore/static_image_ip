f6e276a9d9e019251781546be22f155109407e52
Poseidon <nishant007gore@gmail.com>
2017-10-08 00:58:38 +0530

    all routes to image

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 const port = process.env.PORT || 7000;
 app.use(express.static('public'));
 
-app.get('*', function(req, res){
-	res.redirect('/image.jpg');
-});
+// app.get('*', function(req, res){
+// 	res.redirect('/image.jpg');
+// });
 
 
 app.listen(7000, function(){
