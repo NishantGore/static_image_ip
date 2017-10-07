b36e1d14f6d2955794e4edef986517a9bf4e17c3
Poseidon <nishant007gore@gmail.com>
2017-10-08 00:51:45 +0530

    all routes to image

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 const port = process.env.PORT || 7000;
 app.use(express.static('public'));
 
+app.get('*', function(req, res){
+	res.redirect('/image.jpg');
+});
+
 
 app.listen(7000, function(){
   console.log(`The server is running on port ${port}`);
